aa3aad7fd58aa8b1dbd4df55d8933cc14a8f0e0e
Abdou WADE <117171603+abdouwade1996@users.noreply.github.com>
2024-11-21 12:51:26 +0000

    update of Success.c

--- Success.c
+++ Success.c
@@ -8,21 +8,29 @@
  * It defines the steps and logic required to achieve success.
  * The algorithm can be used as a guide to reach desired goals.
  */
-// Algorithm of Success
+
+// Déclaration de la fonction tryAgain (à définir ailleurs dans le code)
+void tryAgain();
 
 int main(int argc, char *argv[])
 {
+    // Déclaration et initialisation de la variable "noSuccess" à 1
+    int noSuccess = 1;
 
-    int noSuccess = 1; // Declare and initialize the variable "noSuccess"
-
+    // Boucle tant que "noSuccess" est vrai (différent de 0)
     while (noSuccess)
     {
+        // Appel de la fonction tryAgain
         tryAgain();
 
-        int Dead = 0; // Declare and initialize the variable "Dead"
+        // Déclaration et initialisation de la variable "Dead" à 0
+        int Dead = 0;
+
+        // Si "Dead" est vrai (différent de 0), sortir de la boucle
         if (Dead)
             break;
     }
 
-    exit(0);
+    // Terminer le programme avec un code de sortie 0
+    return 0;
 }
